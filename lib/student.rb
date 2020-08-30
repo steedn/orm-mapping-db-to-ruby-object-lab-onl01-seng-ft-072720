1b9fe0f8ebab017f177c96af4a72afaf27ea2d2d
Nathan Steed <steedn@outlook.com>
2020-08-30 18:41:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,8 +6,7 @@ class Student
   end
 
   def self.all
-    # retrieve all the rows from the "Students" database
-    # remember each row should be a new instance of the Student class
+    
   end
 
   def self.find_by_name(name)
